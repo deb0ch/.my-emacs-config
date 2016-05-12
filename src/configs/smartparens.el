0db4c6cafb5c429b0199764b2292f662841d190d
Thomas de Beauchene <thomas.de-beauchene@parrot.com>
2016-05-12 19:49:34 +0200

    added configuration and activation for smartparens

---
+++ src/configs/smartparens.el
@@ -0,0 +1,12 @@
+;; **************************************************************************
+;; *  smartparens: brackets, parenthesis and quotes matching and balancing  *
+;; **************************************************************************
+
+(conf:add-module-to-path "dash")
+(conf:add-module-to-path "smartparens")
+
+(require 'smartparens-config)
+
+(add-hook 'c-mode-hook #'smartparens-mode)
+(add-hook 'c++-mode-hook #'smartparens-mode)
+(add-hook 'js-mode-hook #'smartparens-mode)
--- src/init-config.el
+++ src/init-config.el
@@ -17,6 +17,7 @@
 (conf:load-config "markdown-mode")
 (conf:load-config "markdown-preview-mode")
 (conf:load-config "multiple-cursors")
+(conf:load-config "smartparens")
 (conf:load-config "undo-tree")
 (conf:load-config "ws-butler")
 
