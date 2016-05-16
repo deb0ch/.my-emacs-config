50bdbcbdde5f1931e59740b025024a10fd955215
Thomas de Beauchene <thomas.chauvot-de-beauchene@epitech.eu>
2016-05-16 13:15:04 +0200

    fixed custom monokai linum color definition and cleaned up old useless configs/unused/monokai.el

--- configs/theme-monokai.el
+++ configs/theme-monokai.el
@@ -1,7 +1,8 @@
+
 (conf:add-module-to-path "monokai" 'custom-theme-load-path)
 
 (add-to-list 'custom-theme-load-path (concat conf:root-path "configs/"))
 
 (load-theme 'monokai t)
 
-(custom-set-faces '(linum ((t (:foreground "#75715E")))))
+(set-face-attribute 'linum nil :foreground "#75715E")
--- configs/unused/monokai.el
+++
@@ -1,3 +0,0 @@
-(conf:add-module-to-path "monokai" 'custom-theme-load-path)
-
-(load-theme 'monokai t)
