351ba90f788636cfb5b2d59d5975681a32f5d643
Thomas de Beauchene <thomas.de-beauchene@parrot.com>
2016-05-19 19:14:56 +0200

    fixed compile.sh

--- utils/compile.sh
+++ utils/compile.sh
@@ -4,14 +4,14 @@
 
 cmd="emacs -Q --batch"
 
-directories=$(find src/modules -type d)
+directories=$(find . -type d)
 for directory in $directories
 do
     cmd="$cmd -L $directory"
 done
 
 cmd="$cmd -f batch-byte-compile"
-files=$(find src/modules -name "*.el")
+files=$(find . -name "*.el")
 
 for file in $files
 do
