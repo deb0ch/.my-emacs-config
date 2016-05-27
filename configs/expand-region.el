395e72d6743270a066b56a14395ce93f7e1509cc
Thomas de Beauchene <thomas.de-beauchene@parrot.com>
2016-05-27 17:58:24 +0200

    configs/expand-region: added M-@ keybinding for er/contract-region

--- configs/expand-region.el
+++ configs/expand-region.el
@@ -7,6 +7,7 @@
 (require 'expand-region)
 
 (global-set-key (kbd "M-2") 'er/expand-region)
+(global-set-key (kbd "M-@") 'er/contract-region)
 
 (setq expand-region-contract-fast-key "1")
 (setq expand-region-reset-fast-key "@")
