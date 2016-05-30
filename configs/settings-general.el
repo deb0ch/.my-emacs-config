a5f0cb76d2e0bdc07ff890bafcc7e772d57a96aa
Thomas de Beauchene <thomas.de-beauchene@parrot.com>
2016-05-30 11:14:21 +0200

    configs/setting-general: deactivated show-trailing-whitespace for term modes and buffer modes

--- configs/settings-general.el
+++ configs/settings-general.el
@@ -80,13 +80,12 @@
 ;; Montre les espaces en fin de ligne
 (setq-default show-trailing-whitespace t)
 
+(setq no-ws-mode-hooks '(buffer-menu-mode-hook
+			 eshell-mode-hook
+			 term-mode-hook
+			 term-line-mode-hook
+			 term-char-mode-hook
+			 ansi-term-mode-hook))
 
-
-;; ;; kill ring search
-;; ;; Just call kill-ring-search and enter your search.
-;; ;; M-y and C-y work as usual.  You can also use C-r like in a shell.
-;; ;; C-v, M-v, C-n and C-p will scroll the view.
-;; (autoload 'kill-ring-search "kill-ring-search"
-;;   "Search the kill ring in the minibuffer."
-;;   (interactive))
-;; (global-set-key "\M-\C-y" 'kill-ring-search)
+(dolist (mode-hook no-ws-mode-hooks)
+  (add-hook mode-hook (lambda () (setq show-trailing-whitespace nil))))
