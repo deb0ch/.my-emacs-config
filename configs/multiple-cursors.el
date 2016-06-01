4310280d0c962cc49e5e42b361b771280497909b
Thomas de Beauchene <thomas.de-beauchene@parrot.com>
2016-06-01 19:38:06 +0200

    configs/multiple-cursor: refactored keybindings

--- configs/multiple-cursors.el
+++ configs/multiple-cursors.el
@@ -8,19 +8,32 @@
 (require 'phi-search)
 (require 'multiple-cursors)
 
-(global-set-key (kbd "M-3") #'mc/mark-next-like-this)
-(global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
-
-(global-set-key (kbd "M-#") #'mc/unmark-next-like-this)
-(global-set-key (kbd "M-$") #'mc/unmark-previous-like-this)
-
-(global-set-key (kbd "C-3") #'mc/mark-next-symbol-like-this)
-(global-set-key (kbd "C-4") #'mc/mark-previous-symbol-like-this)
-
-(global-set-key (kbd "C-#") #'mc/unmark-next-symbol-like-this)
-(global-set-key (kbd "C-$") #'mc/unmark-previous-symbol-like-this)
+;; Todo: rebind mc-hide-unmatched-lines-mode
+;; Todo: try mc/mark-next-like-this-extended
+;; Todo: try mc/mark-previous-like-this-symbol
 
 (global-set-key (kbd "M-1") 'mc/mark-all-like-this)
+(global-set-key (kbd "M-!") 'mc/mark-more-like-this-extended)
+
+(global-set-key (kbd "M-3") #'mc/mark-next-like-this)
+(global-set-key (kbd "M-#") #'mc/unmark-next-like-this)
+
+(global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
+(global-set-key (kbd "M-$") #'mc/unmark-previous-like-this)
+
+(global-set-key (kbd "M-5") #'mc/skip-to-next-like-this)
+(global-set-key (kbd "M-%") #'mc/skip-to-previous-like-this)
+
+(global-set-key (kbd "M-6") 'mc/cycle-forward)
+(global-set-key (kbd "M-^") 'mc/cycle-backward)
+
+;; Todo: keybindings do not work in tty
+;(global-set-key (kbd "C-3") #'mc/mark-next-symbol-like-this)
+;(global-set-key (kbd "C-4") #'mc/mark-previous-symbol-like-this)
+
+;; Todo: keybindings do not work in tty
+;(global-set-key (kbd "C-#") #'mc/unmark-next-symbol-like-this)
+;(global-set-key (kbd "C-$") #'mc/unmark-previous-symbol-like-this)
 
 ;; Useless so far
 ;; (global-set-key (kbd "M-5") 'mc/skip-to-next-like-this)
